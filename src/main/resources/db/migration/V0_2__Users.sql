6e31074a14b40a81690f58c050f66aa1c0c7547d
Markus Klinga <mail@mklinga.com>
2022-01-25 17:24:02 +0100

    Persist the db data

--- src/main/resources/db/migration/V0_2__Users.sql
+++ src/main/resources/db/migration/V0_2__Users.sql
@@ -8,16 +8,35 @@ CREATE TABLE IF NOT EXISTS "users" (
     password VARCHAR(500) NOT NULL
 );
 
+INSERT INTO "users" (id, created_at, last_login, username, password) VALUES (
+    nextval('users_id_seq'),
+    '2021-01-25 10:04:00.000Z',
+    NULL,
+    'admin',
+    '$2a$10$zxiLPtS0TZ7sDhCEInn6jOyI/UVGiz2Z9v.TOZRjYKLv4k6vzmwJC');
+
+INSERT INTO "users" (id, created_at, last_login, username, password) VALUES (
+    nextval('users_id_seq'),
+    '2021-01-25 10:04:01.000Z',
+    NULL,
+    'laite',
+    '$2a$10$nFJMUlG6nzgiXfbiTmy66.qc8o7tN0if27kL15PrnHQ9dRAQ40kXa');
+
 CREATE TABLE IF NOT EXISTS "roles" (
-    id SERIAL PRIMARY KEY,
+    id INT PRIMARY KEY,
     role VARCHAR NOT NULL
 );
 
-INSERT INTO "roles" (role) VALUES ('USER');
-INSERT INTO "roles" (role) VALUES ('ADMIN');
+INSERT INTO "roles" (id, role) VALUES (1, 'ADMIN');
+INSERT INTO "roles" (id, role) VALUES (2, 'USER');
 
 CREATE TABLE IF NOT EXISTS "users_roles" (
     user_id INT REFERENCES users (id),
     role_id INT REFERENCES roles (id),
     PRIMARY KEY (user_id, role_id)
 );
+
+INSERT INTO "users_roles" (user_id, role_id) VALUES (1, 1);
+INSERT INTO "users_roles" (user_id, role_id) VALUES (1, 2);
+
+INSERT INTO "users_roles" (user_id, role_id) VALUES (2, 1);
