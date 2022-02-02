f9bdfce04a27f622b653c33f587d70fc2df78d06
Markus Klinga <mail@mklinga.com>
2022-02-02 09:43:20 +0100

    Use podman for dev db

---
+++ db.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+podman run -e POSTGRES_USER=reflekt -e POSTGRES_PASSWORD=secret -e POSTGRES_DB=reflekt -v /home/m/reflekt-postgres-data:/var/lib/postgresql/data -p 5432:5432 --name db postgres
+
