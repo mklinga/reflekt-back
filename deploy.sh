dc5ab90d7d92ea20845a9828c0af3267a5bc7dad
Markus Klinga <mail@mklinga.com>
2022-06-03 11:19:12 +0300

    Add crude deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+mvn clean package -DskipTests
+scp -i ~/reflekt-back-pair.pem target/reflekt-0.0.1-SNAPSHOT.jar ec2-user@ec2-16-171-60-177.eu-north-1.compute.amazonaws.com:~
