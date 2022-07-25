ae189dbcecffd7e6b350f57a51de13c023a5b8a3
Markus Klinga <mail@mklinga.com>
2022-07-25 13:32:11 +0300

    Add unit tests for ContactService

--- src/test/java/com/mklinga/reflekt/contacts/services/ContactServiceTest.java
+++ src/test/java/com/mklinga/reflekt/contacts/services/ContactServiceTest.java
@@ -1,39 +1,38 @@
 package com.mklinga.reflekt.contacts.services;
 
 import static com.mklinga.reflekt.common.TestAuthentication.testUser;
+import static com.mklinga.reflekt.common.configuration.ModelMapperConfiguration.getModelMapper;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.when;
 
 import com.mklinga.reflekt.authentication.model.User;
+import com.mklinga.reflekt.contacts.business.Contact;
 import com.mklinga.reflekt.contacts.dtos.ContactDto;
+import com.mklinga.reflekt.contacts.dtos.ContactRelationDto;
+import com.mklinga.reflekt.contacts.exceptions.ContactExistsException;
 import com.mklinga.reflekt.contacts.model.FullName;
 import com.mklinga.reflekt.contacts.model.JpaContact;
+import com.mklinga.reflekt.contacts.model.RelationPredicate;
 import com.mklinga.reflekt.contacts.repositories.ContactRepository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.jdbc.Sql;
 
 @ExtendWith(MockitoExtension.class)
 class ContactServiceTest {
 
   private final UUID testId1 = UUID.randomUUID();
   private final UUID testId2 = UUID.randomUUID();
-
-  @Autowired
-  private ModelMapper modelMapper;
 
   @Mock
   private ContactRepository contactRepository;
@@ -42,6 +41,7 @@ class ContactServiceTest {
 
   @BeforeEach
   public void setup() {
+    ModelMapper modelMapper = getModelMapper();
     this.contactService = new ContactService(contactRepository, modelMapper);
   }
 
@@ -52,14 +52,98 @@ class ContactServiceTest {
   }
 
   @Test
-  void getAllContacts() {
+  void getAllContactsReturnsAllContactsAsDtos() {
     List<JpaContact> testContacts = getTestContacts();
     when(contactRepository.findAllByOwner(any(User.class))).thenReturn(testContacts);
     List<ContactDto> result = contactService.getAllContacts(testUser());
     assertEquals(2, result.size());
+
+    ContactDto first = result.get(0);
+    assertEquals("Mr.", first.getFirstName());
+    assertEquals("1", first.getLastName());
+    assertEquals(testId1, first.getId());
+
+    ContactDto second = result.get(1);
+    assertEquals("Dr.", second.getFirstName());
+    assertEquals("2", second.getLastName());
+    assertEquals(testId2, second.getId());
   }
 
-  @Test
-  void addContact() {
+  @Nested
+  @DisplayName("addContact")
+  public class AddContactTests {
+
+    private ContactRelationDto createContactRelationDto() {
+      ContactRelationDto contactRelationDto = new ContactRelationDto();
+      contactRelationDto.setId(1);
+      contactRelationDto.setSubject(testId1);
+      contactRelationDto.setObject(testId2);
+      contactRelationDto.setPredicate(RelationPredicate.IS_FRIEND_OF.getCode());
+      return contactRelationDto;
+    }
+
+    @Test
+    void itThrowsExceptionIfDtoIdIsNotDraftId() {
+      User user = testUser();
+      ContactDto newContactDto = new ContactDto();
+      newContactDto.setId(UUID.randomUUID());
+      newContactDto.setFirstName("New FirstName");
+      newContactDto.setLastName("New LastName");
+      newContactDto.setRelations(new ArrayList<>());
+
+      assertThrows(ContactExistsException.class, () -> {
+        contactService.addContact(user, newContactDto);
+      });
+    }
+
+    @Test
+    void itSavesNewContactAndHandlesDraftIdReplacementsAndReturnsIt() {
+      User user = testUser();
+
+      ContactRelationDto contactRelationDto = createContactRelationDto();
+      contactRelationDto.setSubject(Contact.draftId);
+
+      ContactDto newContactDto = new ContactDto();
+      newContactDto.setId(Contact.draftId);
+      newContactDto.setFirstName("New FirstName");
+      newContactDto.setLastName("New LastName");
+      newContactDto.setRelations(List.of(contactRelationDto));
+
+      /* Mocks */
+
+      UUID savedId = UUID.randomUUID();
+      /* When saving DRAFT, we return same item but with ID */
+      when(contactRepository.save(argThat(contact -> contact != null && contact.getId().equals(Contact.draftId))))
+          .thenAnswer(invocation -> {
+            JpaContact argument = invocation.getArgument(0);
+            return new JpaContact(savedId, argument.getFullName(), user, argument.getRelations());
+          });
+
+      when(contactRepository.save(argThat(contact -> contact != null && contact.getId().equals(savedId))))
+          .thenAnswer(invocation -> invocation.getArgument(0));
+
+      List<JpaContact> testContacts = new ArrayList<>(getTestContacts());
+
+      // We add newly saved DRAFT contact to be retrieved from the findAllByOwner, note that for
+      // this draft contact the relations are still empty
+      testContacts.add(new JpaContact(savedId, new FullName("New FirstName", "New LastName"), user, new ArrayList<>()));
+
+      when(contactRepository.findAllByOwner(any(User.class))).thenReturn(testContacts);
+
+      /* Test call */
+      ContactDto result = contactService.addContact(user, newContactDto);
+
+      /* Assertion */
+      assertNotNull(result);
+      assertEquals(savedId, result.getId());
+      assertEquals("New FirstName", result.getFirstName());
+      assertEquals("New LastName", result.getLastName());
+
+      List<ContactRelationDto> savedRelations = result.getRelations();
+      assertEquals(1, savedRelations.size());
+      assertEquals(savedId, savedRelations.get(0).getSubject());
+      assertEquals(contactRelationDto.getObject(), savedRelations.get(0).getObject());
+      assertEquals(contactRelationDto.getPredicate(), savedRelations.get(0).getPredicate());
+    }
   }
 }
